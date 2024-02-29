99dbbb10063ec7f0741778ac164c0afa4f0a7b6e
Dmitriy Isaev <isai1994@mail.ru>
2024-02-29 19:58:24 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Дмитрий Исаев on 29.02.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
